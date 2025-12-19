a696bdc098dd1b2864c0dd5a6165c6e21e515fef
matias <128515180+mti800@users.noreply.github.com>
2025-12-19 12:38:01 -0300

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/mti800/helloworld',
+  site: 'https://mti800.github.io/helloworld',
   base: '/helloworld',
 });
